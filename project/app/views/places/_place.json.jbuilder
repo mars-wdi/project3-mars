06574799a1f063f7d4bc82737ea4ec617d9b906b
Mohammed <moh.jameel178@gmail.com>
2019-03-10 14:20:26 +0300

    Done with places

---
+++ project/app/views/places/_place.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! place, :id, :country, :city, :catogry, :name, :visit, :favorite, :created_at, :updated_at
+json.url place_url(place, format: :json)
---
+++ project/test/system/places_test.rb
@@ -0,0 +1,53 @@
+require "application_system_test_case"
+
+class PlacesTest < ApplicationSystemTestCase
+  setup do
+    @place = places(:one)
+  end
+
+  test "visiting the index" do
+    visit places_url
+    assert_selector "h1", text: "Places"
+  end
+
+  test "creating a Place" do
+    visit places_url
+    click_on "New Place"
+
+    fill_in "Catogry", with: @place.catogry
+    fill_in "City", with: @place.city
+    fill_in "Country", with: @place.country
+    fill_in "Favorite", with: @place.favorite
+    fill_in "Name", with: @place.name
+    fill_in "Visit", with: @place.visit
+    click_on "Create Place"
+
+    assert_text "Place was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Place" do
+    visit places_url
+    click_on "Edit", match: :first
+
+    fill_in "Catogry", with: @place.catogry
+    fill_in "City", with: @place.city
+    fill_in "Country", with: @place.country
+    fill_in "Favorite", with: @place.favorite
+    fill_in "Name", with: @place.name
+    fill_in "Visit", with: @place.visit
+    click_on "Update Place"
+
+    assert_text "Place was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Place" do
+    visit places_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Place was successfully destroyed"
+  end
+end
